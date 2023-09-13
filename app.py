5f06c139be1a6fe3c8190aafb9ee51a46ed54106
FrandenilsonAntony <iabyantony88@gmail.com>
2023-09-13 00:00:38 +0000

    rota e HTML da pagina principal

--- app.py
+++ app.py
@@ -1,6 +1,19 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask("Meu App")
 
+posts = [
+    {
+        "titulo":"Minha primeira postagem",
+        "texto":"teste"
+    },
+    {
+          "titulo":"Segunda postagem",
+          "texto":"Outro teste"
+    }
+
+]
+
 @app.route('/')
-def hello():
-    return "Aplicação em Python com SQLlite.."
+def exibir_entradas():
+    entradas = posts #Mock das postagens
+    return render_template('exibir_entradas.html', entradas=entradas)
