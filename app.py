9d4d65dfccf6c9308cb9c25fcd3d132353e0bf4e
FrandenilsonAntony <iabyantony88@gmail.com>
2023-09-13 18:55:20 +0000

    Toda diretriz do login configurada e pronta

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, session, flash, redirect, url_for
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'pudim'
 
 
 posts = [
@@ -20,6 +21,13 @@ def exibir_entradas():
     entradas = posts #Mock das postagens
     return render_template('exibir_entradas.html', entradas=entradas)
 
-@app.route('/login')
+@app.route('/login', methods=["POST","GET"])
 def login():
-    return render_template('login.html')
+    erro = None
+    if request.method == "POST":
+         if request.form['username'] == "admin" and request.form['password'] == "admin":
+           session['logado'] = True
+           flash("Login efetuado com sucesso!")
+           return redirect(url_for('exibir_entradas'))
+         erro = "Usuário ou senha inválidos"
+    return render_template('login.html', erro=erro)
