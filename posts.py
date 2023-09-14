eaf9356358546cd4cdfaeedf6dc70ca9d6a10ef2
FrandenilsonAntony <iabyantony88@gmail.com>
2023-09-14 02:45:45 +0000

    Completo com Mock e pagina de detalhes

---
+++ posts.py
@@ -0,0 +1,11 @@
+posts = [
+    {
+        "titulo":"Minha primeira postagem",
+        "texto":"teste"
+    },
+    {
+          "titulo":"Segunda postagem",
+          "texto":"Outro teste"
+    }
+
+]
